aa9afe3fcde50ca49e3ce020dac3af3f7229b470
藤原達浩 <t-fujiwara@western-company.com>
2023-05-28 02:05:47 +0900

    コメントを追加

--- microblog/manage.py
+++ microblog/manage.py
@@ -1,8 +1,8 @@
+# バーチャルエンブサーバを立ち上げる際に使用
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
 
 def main():
     """Run administrative tasks."""
